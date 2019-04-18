0b8bc5461575d178d713764e7d10e21509bb03d1
黄卫华 <48366939@qq.com>
2019-04-18 11:18:59 +0800

    初始版本，
    初始版本，

---
+++ firstaar/src/main/java/com/xmobile/firstaar/FirstAARActivity.java
@@ -0,0 +1,13 @@
+package com.xmobile.firstaar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstAARActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_aar);
+    }
+}
